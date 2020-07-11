7b50b4ed3b47e4e35ffcd90b119c8e222e0d7349
Sharad Raj <36638057+sharadcodes@users.noreply.github.com>
2020-07-11 18:24:34 +0530

    Add files via upload

---
+++ routes/question.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+	getAllQuestions,
+	createQuestion,
+	getQuestion,
+	getQuestionById,
+	removeQuestion,
+	updateQuestion,
+	pushAnswerInAnswersList,
+} = require("../controllers/question");
+
+const { isSignedIn, isAuthenticated } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
+
+// params
+router.param("userId", getUserById);
+router.param("questionId", getQuestionById);
+
+// actual routes
+// create
+router.post(
+	"/question/create/:userId",
+	isSignedIn,
+	isAuthenticated,
+	createQuestion
+);
+// read
+router.get("/question/:questionId", getQuestion);
+router.get("/questions", getAllQuestions);
+// update
+router.put(
+	"/question/:questionId/:userId",
+	isSignedIn,
+	isAuthenticated,
+	updateQuestion
+);
+
+router.put(
+	"/question/:questionId/answers/:userId",
+	isSignedIn,
+	isAuthenticated,
+	pushAnswerInAnswersList
+);
+
+// delete
+router.delete(
+	"/question/:questionId/:userId",
+	isSignedIn,
+	isAuthenticated,
+	removeQuestion
+);
+module.exports = router;
