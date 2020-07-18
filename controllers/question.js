9f25fa9c8c12fa062f129d4e0a4d7f8c983501b0
Sharad Raj <36638057+sharadcodes@users.noreply.github.com>
2020-07-19 00:14:27 +0530

    Update question.js

--- controllers/question.js
+++ controllers/question.js
@@ -33,7 +33,7 @@ exports.getQuestion = (req, res) => {
 exports.getAllQuestions = (req, res) => {
 	Question.find()
 		.populate("user_id", "_id name")
-		.populate({ path: "answers._id", "_id name", model: 'Answer' })
+		.populate("answers._id", "_id name")
 		.populate("category", "_id name")
 		.exec((err, question) => {
 			if (err) {
